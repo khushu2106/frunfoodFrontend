925b071b33c0d0543fe66b3d7c5095b9d933b7f6
Khushbu <guptakhushu2106@gmail.com>
2026-01-10 11:42:20 +0530

    Add layout

---
+++ src/Components/Pages/Auth/Login.jsx
@@ -0,0 +1,15 @@
+import { react } from 'react'
+
+
+function App(){
+    return (
+        <div className='container'>
+            <div className='form-container'>
+                <div className='form-toggle'></div>
+                <button>Login</button>
+            </div>
+        </div>
+    )
+}
+
+export default Login
