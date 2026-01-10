0c16c2031ff9a05c38fd73a11500973c7c34e113
Khushbu <guptakhushu2106@gmail.com>
2026-01-10 16:51:09 +0530

    Update App.jsx
    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,35 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import './App.css'
-import HeroHome from './Components/Home/HeroHome/HeroHome'
-import Navbar from './Components/Common/Navbar'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // BrowserRouter ko as Router import kiya
+import './App.css';
+import HeroHome from './Components/Home/HeroHome/HeroHome';
+import HeroSection from './Components/Home/HeroHome/HeroSection';
+import Navbar from './Components/Common/Navbar/Navbar';
+import ProductList from './Components/Home/Products/ProductList';
+import ProductDetails from './Components/Home/Products/ProductDetails';
+import CategoryProduct from './Components/Home/Products/CategoryProduct';
+import Aboutus from './Components/Home/Aboutus/Aboutus';
 // import Login from './Components/Pages/Auth/Login'
 
 function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <Navbar />
+      {/* Routes tag ke andar hi saare Route honge */}
       <Routes>
-        <Route path="/hero" element={<HeroHome />} />
+        <Route path='/' element={
+          <>
+            <HeroHome />
+            <HeroSection />
+            <CategoryProduct />
+            <ProductList />
+          </>
+        } />
+        
+        <Route path='/shop' element={<CategoryProduct />} />
+        <Route path='/product/:id' element={<ProductDetails />} />
+        <Route path='/about' element={<Aboutus />} />
+        {/* <Route path='/contact' element={<CategoryProduct />}/> */}
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 }
 
