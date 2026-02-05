35f71827f05ef44258e21494a80979f364a4d2ba
Khushbu <guptakhushu2106@gmail.com>
2026-02-05 18:57:59 +0530

    correct css file link

--- src/Components/Pages/Order/OrderDetails.jsx
+++ src/Components/Pages/Order/OrderDetails.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useParams, useNavigate } from "react-router-dom";
-import "./OrderDetails.css";
+import "./Orders.css";
 
 const OrderDetails = () => {
   const { orderId } = useParams();
